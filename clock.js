394c0468f2c00d97b6c54791d04d89ec86a2ae5d
Philippe Lanougadere <soufiane92230@gmail.com>
2020-11-12 10:45:50 +0100

    Put transition effect back - Add condition to improve hands behavior

--- clock.js
+++ clock.js
@@ -1,22 +1,36 @@
 
+const hand = document.querySelectorAll('.hand');
 const secondHand = document.querySelector('.second');
 const hourHand = document.querySelector('.hour');
 const minuteHand = document.querySelector('.minute');
 
 const secondRunning = () => {
+
+    // how many degres for each second
     const currentSecond = new Date().getSeconds();
     const degreSecond = (currentSecond * 360 / 60) + 90;
-    secondHand.style.transform = `rotate(${degreSecond}deg)`;
 
+    // how many degres for each minute
     const currentMinute = new Date().getMinutes();
     const degreMinute = (currentMinute * 360 / 60) + 90;
-    minuteHand.style.transform = `rotate(${degreMinute}deg)`;
+    
+    // how many degres for each hour
+    const currentHour = (new Date().getHours() % 12);  // % 12 to get 12h format instead of 24h
+    const degreHour = (currentHour *360 / 12) + 90; 
+    
+    // to avoid clock hand to make a complete run each time hand hits 90 degres
+    if (degreSecond == 90 || minuteHand == 90 || hourHand == 90) {
+        hand.forEach(item => item.classList.add("no-transition")) 
+    } 
+    else {
+        hand.forEach(item => item.classList.remove("no-transition")) 
+    }
 
-    const currentHour = (new Date().getHours() % 12);
-    const degreHour = (currentHour *360 / 12) + 90;
-    hourHand.style.transform = `rotate(${degreHour}deg)`;
-
-    console.log(currentHour);
+    // make each hand runs his own degres rotation
+    secondHand.style.transform = `rotate(${degreSecond}deg)`;  
+    minuteHand.style.transform = `rotate(${degreMinute}deg)`;    
+    hourHand.style.transform = `rotate(${degreHour}deg)`;  
+    
 }
 
 setInterval(secondRunning, 1000)
